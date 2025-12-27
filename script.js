da098a47f9c8fc938c3e15f9814848f8f0b889da
manohar <manohargadudhula76@gmail.com>
2025-12-27 20:04:31 +0530

    Update portfolio description paragraph

--- script.js
+++ script.js
@@ -11,24 +11,3 @@ document.querySelectorAll(".menu-bar a").forEach(link => {
     mobileNav.classList.remove("active");
   });
 });
-const sections = document.querySelectorAll(
-  '.sec1,.sec2,.sec3,.sec4,.sec5'
-);
-
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('sec-show');   // animate IN
-      } else {
-        entry.target.classList.remove('sec-show'); // reset when OUT
-      }
-    });
-  },
-  {
-    threshold: 0.2
-  }
-);
-
-sections.forEach(section => observer.observe(section));
-
